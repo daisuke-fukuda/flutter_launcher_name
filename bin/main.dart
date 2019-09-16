2ed5f96415c0bab1185ab0d374ec4cc97e7b58f3
daisuke-fukuda <32258949+daisuke-fukuda@users.noreply.github.com>
2019-09-16 16:22:59 +0900

    enable exec "flutter pub run flutter_launcher_name:main"

---
+++ bin/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_launcher_name/flutter_launcher_name.dart'
+    as FlutterName;
+
+main(List<String> arguments) {
+  FlutterName.exec();
+}
--- lib/flutter_launcher_name.dart
+++ lib/flutter_launcher_name.dart
@@ -1,7 +1,5 @@
 library flutter_launcher_name;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+exec() {
+  print('flutter_launcher_name exec');
 }
