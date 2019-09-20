66ff3d28f9b50ec9d402ca0ac41dd7c705a19cb0
daisuke-fukuda <32258949+daisuke-fukuda@users.noreply.github.com>
2019-09-20 18:39:22 +0900

    get launcher name from pubspec.yaml

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,3 @@
 const String androidManifestFile = 'android/app/src/main/AndroidManifest.xml';
 const String iOSInfoPlistFile = 'ios/Runner/Info.plist';
+const String yamlKey = 'flutter_launcher_name';
--- lib/flutter_launcher_name.dart
+++ lib/flutter_launcher_name.dart
@@ -1,16 +1,39 @@
 library flutter_launcher_name;
 
+import 'dart:io';
+
 import 'package:flutter_launcher_name/android.dart' as android;
+import 'package:flutter_launcher_name/constants.dart' as constants;
 import 'package:flutter_launcher_name/ios.dart' as ios;
+import 'package:yaml/yaml.dart';
 
 exec() {
   print('start');
 
-  // TODO(daisuke-fukuda): get from pubspec
-  final newName = 'newName';
+  final config = loadConfigFile();
+
+  final newName = config['name'];
 
   android.overwriteAndroidManifest(newName);
   ios.overwriteInfoPlist(newName);
 
   print('exit');
+}
+
+Map<String, dynamic> loadConfigFile() {
+  final File file = File('pubspec.yaml');
+  final String yamlString = file.readAsStringSync();
+  final Map yamlMap = loadYaml(yamlString);
+
+  if (yamlMap == null || !(yamlMap[constants.yamlKey] is Map)) {
+    throw new Exception('flutter_launcher_name was not found');
+  }
+
+  // yamlMap has the type YamlMap, which has several unwanted sideeffects
+  final Map<String, dynamic> config = <String, dynamic>{};
+  for (MapEntry<dynamic, dynamic> entry in yamlMap[constants.yamlKey].entries) {
+    config[entry.key] = entry.value;
+  }
+
+  return config;
 }
